d703ab645cf878346399f6e56bd088a19ddd9e19
Jaehun <qkdwogns98@gmail.com>
2024-05-01 12:30:03 +0900

    Props Object 정의

--- src/app/register/RegisterUI.tsx
+++ src/app/register/RegisterUI.tsx
@@ -1,3 +1,45 @@
-export default function RegisterUI() {
-  return <></>;
+import { FormEventHandler } from "react";
+
+type Props = {
+  name: string;
+  password: string;
+  onChangeName: () => void;
+  onChangePassword: () => void;
+  onSubmit: FormEventHandler<HTMLFormElement>;
+};
+
+export default function RegisterUI({
+  name,
+  password,
+  onChangeName,
+  onChangePassword,
+  onSubmit,
+}: Props) {
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <label>
+          <input
+            type="text"
+            placeholder="NAME"
+            name="name"
+            value={name}
+            onChange={onChangeName}
+          />
+        </label>
+
+        <label>
+          <input
+            type="text"
+            placeholder="PASSWORD"
+            name="password"
+            value={password}
+            onChange={onChangePassword}
+          />
+        </label>
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
